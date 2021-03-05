2dad2e7496db1bf886102f173776378f76c0ec38
ODOMI-IGWEJI KELLY <75096682+ODOMI1234@users.noreply.github.com>
2021-03-05 09:46:30 -0800

    Add files via upload

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 //Declare const
 const FIRST_NAME = document.querySelector("#firstName");
-const LAST_NAME = document.querySelector("secondName");
+const LAST_NAME = document.querySelector("#secondName");
 const CLIENT_WEIGHT = document.querySelector("#weight");
 const CLIENT_HEIGHT = document.querySelector("#height");
 const CLIENT_RESULT = document.querySelector(".Result");
@@ -9,7 +9,7 @@ const CALC_BTM_BTN =  document.querySelector(".btn");
 //Add an event listener to the button
 CALC_BTM_BTN.addEventListener("click", () =>{
      const FIRST_NAME = document.querySelector("#firstName");
-     const LAST_NAME = document.querySelector("secondName");
+     const LAST_NAME = document.querySelector("#secondName");
      const CLIENT_WEIGHT = document.querySelector("#weight");
      const CLIENT_HEIGHT = document.querySelector("#height");
      const CLIENT_RESULT = document.querySelector(".Result");
@@ -20,8 +20,7 @@ let BMI = (CLIENT_WEIGHT.value) / (CLIENT_HEIGHT.value**2);
 //Let us set the BMI value to two decimal place.
   BMI = BMI.toFixed(2);
 
-//Let us set the BMI value to two decimal place.
-    BMI = BMI.toFixed(2);
+
 
   //Form Validation
      if (FIRST_NAME.value=="" || LAST_NAME.value=="" || CLIENT_HEIGHT.value=="" || CLIENT_WEIGHT.value=="") {
@@ -34,16 +33,42 @@ let BMI = (CLIENT_WEIGHT.value) / (CLIENT_HEIGHT.value**2);
               }, 1500);              
           }, 100);
      } else {
-      CLIENT_RESULT.textContent = `${FIRST_NAME.value} ${LAST_NAME.value} your BMI value is ${BMI}kg/m. Thank for using this app.`;
-      CLIENT_RESULT.style.color = "black";
-      CLIENT_RESULT.style.fontStyle = "normal"; 
+
+    if(BMI > 0 && BMI < 18) {
+            CLIENT_RESULT.textContent = `${FIRST_NAME.value} ${LAST_NAME.value} your BMI value is ${BMI}kg/m. You are UNDERWEIGHT. You may want to see a doctor. Thank you for using Kelly's BMI Calculator`;
+            CLIENT_RESULT.style.color = "blue";
+            CLIENT_RESULT.style.fontStyle = "normal";
+            CLIENT_RESULT.style.fontWeight = "bold";
+            init();
+
+        } else if (BMI > 18 && BMI < 25) {
+            CLIENT_RESULT.textContent = `${FIRST_NAME.value} ${LAST_NAME.value} your BMI value is ${BMI}kg/m. You are NORMAL. Cheers! Thank you for using Kelly's BMI Calculator`;
+            CLIENT_RESULT.style.color = "green";
+            CLIENT_RESULT.style.fontStyle = "normal";
+            CLIENT_RESULT.style.fontWeight = "bold";
+            init();
+
+        } else if (BMI > 25 && BMI < 30) {
+            CLIENT_RESULT.textContent = `${FIRST_NAME.value} ${LAST_NAME.value} your BMI value is ${BMI}kg/m. You are OVERWEIGHT. You need to see a medical doctor. Thank you for using Kelly's BMI Calculator`;
+            CLIENT_RESULT.style.color = "orange";
+            CLIENT_RESULT.style.fontStyle = "normal";
+            CLIENT_RESULT.style.fontWeight = "bold";
+            init();
+
+        } else {
+            CLIENT_RESULT.textContent = `${FIRST_NAME.value} ${LAST_NAME.value} your BMI value is ${BMI}kg/m. You are OBESE. You need to see a medical doctor URGENTLY. Thank you for using Kelly's BMI Calculator`;
+            CLIENT_RESULT.style.color = "red";
+            CLIENT_RESULT.style.fontStyle = "normal";
+            CLIENT_RESULT.style.fontWeight = "bold";
+            init();
+            }
      }
 });
 
 
 function init () {
-     FIRST_NAME.value=="";
-     LAST_NAME.value=="";
-     CLIENT_HEIGHT.value=="";
-     CLIENT_WEIGHT.value=="";
+     FIRST_NAME.value="";
+     LAST_NAME.value="";
+     CLIENT_HEIGHT.value="";
+     CLIENT_WEIGHT.value="";
 }
